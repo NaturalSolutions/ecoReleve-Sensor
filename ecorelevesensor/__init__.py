f1f705c714b1e5f80de2345ec63f00dee72521a0
thomaspeel <peel.thomas@gmx.fr>
2014-05-15 18:08:14 +0200

    Changes in development.ini

--- ecorelevesensor/__init__.py
+++ ecorelevesensor/__init__.py
@@ -1,5 +1,5 @@
 from pyramid.config import Configurator
-from sqlalchemy import engine_from_config, create_engine
+from sqlalchemy import engine_from_config
 
 from .models import (
     DBSession,
@@ -11,14 +11,12 @@ def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
     engine = engine_from_config(settings, 'sqlalchemy.')
-    #engine = create_engine("mssql+pyodbc://eReleveApplication:123456@localhost\\SQLSERVER2008/ECWP_eReleve_Sensor?driver=SQL SERVER")
-    print engine
     DBSession.configure(bind=engine)
     Base.metadata.bind = engine
     config = Configurator(settings=settings)
     config.include('pyramid_chameleon')
     config.add_static_view('static', 'static', cache_max_age=3600)
-    config.add_route('home', '/')
-    config.add_route('weekData', '/weekData')
+    config.add_route('home', '/ecoReleve-Sensor')
+    config.add_route('weekData', 'ecoReleve-Sensor/weekData')
     config.scan()
     return config.make_wsgi_app()
