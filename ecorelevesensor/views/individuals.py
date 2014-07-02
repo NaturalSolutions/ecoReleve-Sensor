fed3770fd482f55cbc5bdf600a3af4600d19cc3f
thomaspeel <peel.thomas@gmx.fr>
2014-07-02 08:55:22 +0200

    Code refactoring : individual search

--- ecorelevesensor/views/individuals.py
+++ ecorelevesensor/views/individuals.py
@@ -18,7 +18,7 @@ def core_individuals_values(request):
       return []
 
 @view_config(route_name=route_prefix + 'search', renderer='json', request_method='POST')
-def core_individuals_stations(request):
+def core_individuals_search(request):
    try:
       query = select(V_Search_Indiv.c)
       # Look over the criteria list
@@ -33,33 +33,21 @@ def core_individuals_stations(request):
          query = query.limit(limit)
       if offset > 0:
          offset = query.offset(offset)
-      # Set sorting column and order
+      # Set sorting columns and order
       order_by = request.json_body.get('order_by', {})
+      order_by_clause = []
       for column, order in order_by.items():
          if column in V_Search_Indiv.columns:
             if order == 'asc':
-               query = query.order_by(V_Search_Indiv.columns[column].asc())
+               order_by_clause.append(V_Search_Indiv.columns[column].asc())
             elif order == 'desc':
-               query = query.order_by(V_Search_Indiv.columns[column].desc())
-
+               order_by_clause.append(V_Search_Indiv.columns[column].desc())
+      if len(order_by_clause) > 0:
+         query = query.order_by(*order_by_clause)
+      # Run query
       result = []
       for row in DBSession.execute(query).fetchall():
-         result.append(
-            {'id':row[V_Search_Indiv.c.id],
-             'age':row[V_Search_Indiv.c.age],
-             'specie':row[V_Search_Indiv.c.specie],
-             'ptt':row[V_Search_Indiv.c.ptt],
-             'sex':row[V_Search_Indiv.c.sex],
-             'origin':row[V_Search_Indiv.c.origin],
-             'monitoringStatus':row[V_Search_Indiv.c.monitoringStatus],
-             'surveyType':row[V_Search_Indiv.c.surveyType],
-             'releaseArea':row[V_Search_Indiv.c.releaseArea],
-             'releaseYear':row[V_Search_Indiv.c.releaseYear],
-             'captureArea':row[V_Search_Indiv.c.captureArea],
-             'captureYear':row[V_Search_Indiv.c.captureYear]
-            }
-         )
+         result.append({column:row[column] for column in row.keys()})
       return result
    except:
-      raise
       return []
