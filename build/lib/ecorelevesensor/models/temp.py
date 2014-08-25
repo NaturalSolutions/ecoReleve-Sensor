afc81160447c434979b626eb822f6e5098fc85b4
thomaspeel <peel.thomas@gmx.fr>
2014-08-25 11:56:48 +0200

    Delete build and dist directory.

--- build/lib/ecorelevesensor/models/temp.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import Column, Integer, Table, Sequence, String
-from ecorelevesensor.models import Base
-
-# Specific SQL Server 
-spatial_table = Table('spatial_temp', Base.metadata,
-   Column('id', Integer, Sequence('spatial_temp_pk_id'), primary_key = True),
-   Column('geo', String)
-)
